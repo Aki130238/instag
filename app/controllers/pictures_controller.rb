db449ac4334a167d3d99ef927920e2a01202ea5a
Akira <autumn.no7@gmail.com>
2019-07-29 11:51:18 +0900

    投稿機能追加

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,2 +1,20 @@
 class PicturesController < ApplicationController
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    if @picture.save
+        redirect_to new_picture_path, notice: "Pictureを作成しました！"
+      else
+        render 'new'
+      end
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:title, :content, :image_pict, :image_cache)
+  end
 end
