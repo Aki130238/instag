912235119ccb4cffb2aa8d14ca8375f10625d59d
Akira <autumn.no7@gmail.com>
2019-07-29 17:01:30 +0900

    ユーザー機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_29_021017) do
+ActiveRecord::Schema.define(version: 2019_07_29_072006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2019_07_29_021017) do
     t.string "title"
     t.text "content"
     t.string "image_pict"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "image_user"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
